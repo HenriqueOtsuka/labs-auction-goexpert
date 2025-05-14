9151ea960c5c9d52576b0fd1cf18665969951f1b
HenriqueOtsuka <henrique-ots@hotmail.com>
2025-05-14 00:19:29 -0300

    using ticker to calculate time

--- internal/infra/database/auction/create_auction.go
+++ internal/infra/database/auction/create_auction.go
@@ -5,7 +5,10 @@ import (
 	"fullcycle-auction_go/configuration/logger"
 	"fullcycle-auction_go/internal/entity/auction_entity"
 	"fullcycle-auction_go/internal/internal_error"
+	"os"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -30,7 +33,14 @@ func NewAuctionRepository(database *mongo.Database) *AuctionRepository {
 
 func (ar *AuctionRepository) CreateAuction(
 	ctx context.Context,
-	auctionEntity *auction_entity.Auction) *internal_error.InternalError {
+	auctionEntity *auction_entity.Auction,
+) *internal_error.InternalError {
+	intervalStr := os.Getenv("AUCTION_INTERVAL")
+	duration, err := time.ParseDuration(intervalStr)
+	if err != nil || duration <= 0 {
+		logger.Error("Invalid or Incorrect AUCTION_INTERVAL, defaulting to 1m", err)
+		duration = time.Minute
+	}
 	auctionEntityMongo := &AuctionEntityMongo{
 		Id:          auctionEntity.Id,
 		ProductName: auctionEntity.ProductName,
@@ -40,11 +50,22 @@ func (ar *AuctionRepository) CreateAuction(
 		Status:      auctionEntity.Status,
 		Timestamp:   auctionEntity.Timestamp.Unix(),
 	}
-	_, err := ar.Collection.InsertOne(ctx, auctionEntityMongo)
+	_, err = ar.Collection.InsertOne(ctx, auctionEntityMongo)
 	if err != nil {
 		logger.Error("Error trying to insert auction", err)
 		return internal_error.NewInternalServerError("Error trying to insert auction")
 	}
+
+	go func(id string) {
+		timer := time.NewTimer(duration)
+		<-timer.C
+
+		filter := bson.M{"_id": id, "status": auction_entity.Active}
+		update := bson.M{"$set": bson.M{"status": auction_entity.Completed}}
+		if _, err := ar.Collection.UpdateOne(context.Background(), filter, update); err != nil {
+			logger.Error("Error to close auction", err)
+		}
+	}(auctionEntity.Id)
 
 	return nil
 }
